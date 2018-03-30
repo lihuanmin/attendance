634526e622b2e1c17b12deead9caff1077b9d034
lihuanmin <964517471@qq.com>
2018-03-30 17:36:36 +0800

    Modified common.js file and  add time format conversion function

--- src/main/webapp/static/js/commons/common.js
+++ src/main/webapp/static/js/commons/common.js
@@ -3,3 +3,8 @@ function getUrl(url) {
     var postPath = strPath.substring(0, strPath.substring(1).indexOf("/")+1);
     return postPath+"/"+url;
 }
+function getDate(date){
+	var time = new Date(parseInt(date))
+	var result = time.getFullYear()+"-"+time.getMonth() +"-"+time.getDate()
+	return result;
+}
